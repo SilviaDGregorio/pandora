a386922003c84cd39c80153060d75b326d02e35d
unknown <ydril.linwelin@gmail.com>
2015-05-06 20:04:23 +0200

    paused game and movement enemy

---
+++ Assets/Scripts/pause.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+var TS : float; //time scale
+var paused :boolean; //pause or unpause the game
+
+function Start(){
+	TS = Time.timeScale;
+}
+function Update(){
+	if(Input.GetKeyUp("p")){
+		if(!paused){
+			paused=true;
+		}
+		else{
+			paused=false;
+		}
+	}
+	if(paused){
+		if(Time.timeScale >0){
+			Time.timeScale  =0;
+			audio.Pause();
+		}
+	}
+	else{
+		if(Time.timeScale < TS){
+			Time.timeScale  =TS;
+			audio.Play();
+		}
+	}
+}
