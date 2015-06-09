a44c029cef78e48b0342f96bc5fafe0d71bc62ef
unknown <ydril.linwelin@gmail.com>
2015-06-09 11:39:37 +0200

    fixed bug spawpoint and boxcollider boxes

--- Assets/Scripts/Light_on_off.js
+++ Assets/Scripts/Light_on_off.js
@@ -9,18 +9,24 @@ var light_on_off =GameObject.Find("Point light");
 	if(Input.GetKeyUp("e")){
 		if(light_on_off.light.intensity!=0){
 			light_on_off.light.intensity =0;
+			size_box_collider(0);
 		}
 		else{
 			light_on_off.light.intensity =4;
+			size_box_collider(1);
 		}
-		var cube_when_light_on_array = GameObject.FindGameObjectsWithTag("Cube_hide_when_light_on");
-	
-		for(var cube : GameObject in cube_when_light_on_array){
-			
-			var box_col = cube.collider as BoxCollider;
-			box_col.size= new Vector3(0,0,0);
-		}
+
 		
 
 	}
+}
+function size_box_collider(size_box:int){
+	var cube_when_light_on_array = GameObject.FindGameObjectsWithTag("Cube_hide_when_light_on");
+
+	for(var cube : GameObject in cube_when_light_on_array){
+		
+		var box_col = cube.collider as BoxCollider;
+		box_col.size= new Vector3(size_box,size_box,size_box);
+	}
+
 }
--- Assets/Scripts/move_camera_with_player.js
+++ Assets/Scripts/move_camera_with_player.js
@@ -6,15 +6,18 @@ var smoothRotation = true;
 var rotationDamping = 10.0;
 
 function Update () {
-	var wantedPosition = target.TransformPoint(0, height, -distance);
-	transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
+	
+		var wantedPosition = target.TransformPoint(0, height, -distance);
+		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
 
-	if (smoothRotation) {
-		var wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
-		transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
-	}
+		if (smoothRotation) {
+			var wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+		}
 
-	else transform.LookAt (target, target.up);
-	//Hace que nuestra camara no nos gire y veamos el suelo cuando saltamos
-	transform.localRotation=Quaternion.Euler(0,0,0);
+		else transform.LookAt (target, target.up);
+		//Hace que nuestra camara no nos gire y veamos el suelo cuando saltamos
+		transform.localRotation=Quaternion.Euler(0,0,0);
+	
+
 }
