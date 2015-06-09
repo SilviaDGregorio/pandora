76a90a915e6813d7aba1e24aaa942cca293f7bf2
unknown <ydril.linwelin@gmail.com>
2015-06-09 20:05:01 +0200

    added box_hide_when_light_on

--- Assets/Scripts/Light_on_off.js
+++ Assets/Scripts/Light_on_off.js
@@ -6,14 +6,17 @@ function Start () {
 
 function Update () {
 var light_on_off =GameObject.Find("Point light");
+
 	if(Input.GetKeyUp("e")){
 		if(light_on_off.light.intensity!=0){
 			light_on_off.light.intensity =0;
 			size_box_collider(0);
+			hide_box(true,1);
 		}
 		else{
 			light_on_off.light.intensity =4;
 			size_box_collider(1);
+			hide_box(false,0);
 		}
 
 		
@@ -21,10 +24,21 @@ var light_on_off =GameObject.Find("Point light");
 	}
 }
 function size_box_collider(size_box:int){
+	var cube_when_light_off_array = GameObject.FindGameObjectsWithTag("Cube_hide_when_light_off");
+
+	for(var cube : GameObject in cube_when_light_off_array){
+		
+		var box_col = cube.collider as BoxCollider;
+		box_col.size= new Vector3(size_box,size_box,size_box);
+	}
+
+}
+function hide_box(hide_or_not:boolean,size_box:int){
 	var cube_when_light_on_array = GameObject.FindGameObjectsWithTag("Cube_hide_when_light_on");
 
 	for(var cube : GameObject in cube_when_light_on_array){
-		
+		cube.renderer.enabled=hide_or_not;
+			
 		var box_col = cube.collider as BoxCollider;
 		box_col.size= new Vector3(size_box,size_box,size_box);
 	}
--- Assets/Scripts/ScriptsReader/Reader_js.js
+++ Assets/Scripts/ScriptsReader/Reader_js.js
@@ -3,11 +3,14 @@
 var csv : TextAsset; 
 var cube : GameObject;
 var cube_red :GameObject;
+var normal_enemy :GameObject;
+var cube_hide_light_on :GameObject;
 function Start ()
 {
 	var map=CSVReader.SplitCsvGrid(csv.text);
 	var tamX=CSVReader.getWidth(csv.text);
 	var tamY =CSVReader.getHeight(csv.text);
+	
 	var xPosition=0;
 	var yPosition=0;
 	
@@ -17,24 +20,24 @@ function Start ()
 			switch(position){		
 			
 				case "1":
-				Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
+				//Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
 					Instantiate(cube_red as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0), Quaternion.identity);
 					break;
 				case "2":
-					Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
+					//Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
 					Instantiate(cube as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0), Quaternion.identity);
 					break;
 				case "3":
-					Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);		
+					//Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);		
 					Instantiate(cube as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0), Quaternion.identity);
 					break;
 				case "4":
-					Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
-					Instantiate(cube as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0), Quaternion.identity);
+					//Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
+					Instantiate(cube_hide_light_on as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0), Quaternion.identity);
 					break;
 				case "5":
-					Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);	
-					Instantiate(cube as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0), Quaternion.identity);
+				//	Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);
+					Instantiate(normal_enemy as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0.4947718), Quaternion.Euler(Vector3(0,180,0)));
 					break;
 			}
 			
