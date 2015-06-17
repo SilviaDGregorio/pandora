4bbca1c6319b49d43fbff73fdb1702efa0e5d41e
unknown <ydril.linwelin@gmail.com>
2015-06-17 13:05:20 +0200

    added parallax

---
+++ Assets/Scripts/parallax/Parallax_background.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+private var position_camera_first :float;
+public var direction_with_player : boolean;
+//Cuanto se va a desplazar respecto a lo que se desplace la camara
+public var offset :int;
+public var MainCamera : GameObject;
+function Start () {
+	position_camera_first = MainCamera.transform.position.x;
+
+}
+
+function Update () {
+	if(direction_with_player){
+		transform.position.x=( MainCamera.transform.position.x -position_camera_first)/offset;
+
+	}
+	else{
+		transform.position.x=(position_camera_first - MainCamera.transform.position.x)/offset;
+
+	}
+}
---
+++ Assets/Scripts/parallax/parallax_background_vertical.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+private var target : GameObject;
+var offset :float;
+private var position_Y_new :float;
+private var position_Y_old:float;
+
+function Start () {
+	position_Y_new= target.transform.position.y;
+	position_Y_old=0.0;
+	
+}
+
+function Update () {
+	
+		target = GameObject.FindWithTag("Player");
+		position_Y_new=target.transform.position.y;
+		if (Mathf.Abs(position_Y_new-position_Y_old)>=3){
+			Debug.Log(position_Y_new);
+			Debug.Log(position_Y_old);
+			Debug.Log(Mathf.Abs(position_Y_new-position_Y_old)>=3);			
+			Debug.Log("background last "+transform.position.y);
+			transform.position.y = transform.position.y+((position_Y_new-position_Y_old)/offset);
+			position_Y_old=position_Y_new;
+			Debug.Log("background new "+transform.position.y);
+		}	
+}
