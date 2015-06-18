502ddf6dbaa278fb0211d9d565a2651f086e27ca
unknown <ydril.linwelin@gmail.com>
2015-06-18 12:22:02 +0200

    player dead if stay inside box when light on

--- Assets/Scripts/box_collider_enemy.js
+++ Assets/Scripts/box_collider_enemy.js
@@ -1,10 +1,23 @@
 ﻿#pragma strict
-
+var Player : GameObject;
+var spawnPoint : Transform;
 function OnTriggerEnter(other : Collider){
-	
-	if(other.tag == "Enemy"){
+var cube = this.gameObject;
+	if( other.tag == "Enemy" ){
 	var enemy_move=other.GetComponent(Enemy_movement);
 		enemy_move.direction=enemy_move.direction*-1;
 	}
 	
+}
+function OnTriggerStay(other: Collider) {
+var light_on_off =GameObject.Find("Point light");
+
+
+
+	if(light_on_off.light.intensity!=0 && other.tag == "Player" ){
+			Destroy(other.gameObject);
+			var P : GameObject = Instantiate(Player, spawnPoint.position, Quaternion.identity);
+			var sf = Camera.main.GetComponent(move_camera_with_player);
+			sf.target = P.transform;
+	}
 }
