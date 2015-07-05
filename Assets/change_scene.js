6e92806a655141ae51bcf729b37fe8cc64de9275
unknown <ydril.linwelin@gmail.com>
2015-07-05 18:40:33 +0200

    fixed settings problem with exe

--- Assets/change_scene.js
+++ Assets/change_scene.js
@@ -1,8 +1,12 @@
 ﻿#pragma strict
+var lvl_file : TextAsset; 
+var settings_file : TextAsset; 
+private var keys = new String[4]; //front, back,space ,power
 private var lvl:int;
 private var max_lvl:int;
 function Start () {
-
+	isCreatedSettingsFile();
+	isCreatedLvlFile();
 }
 
 function Update () {
@@ -51,8 +55,48 @@ function Read(){
        sr.Close();
     }
     catch (e) {
-        // Let the user know what went wrong.
+         // Let the user know what went wrong.
         print("The file could not be read:");
         print(e.Message);
+
     }
 }
+function isCreatedLvlFile(){
+	try {
+        // Create an instance of StreamReader to read from a file.
+       var sr = new StreamReader("Assets/Settings/Lvl.txt");
+       sr.Close();
+    }
+    catch (e) {
+        // Let the user know what went wrong.
+        var texto = lvl_file.text.Split("-"[0]);
+		lvl=parseInt(texto[0]);
+		max_lvl=parseInt(texto[1]);
+		Write();
+    }
+}
+function isCreatedSettingsFile(){
+	try {
+        // Create an instance of StreamReader to read from a file.
+       var sr = new StreamReader("Assets/Settings/Settings.txt");
+       sr.Close();
+    }
+    catch (e) {
+        // Let the user know what went wrong.
+        keys = settings_file.text.Split("-"[0]);        
+        WriteSettings();
+		
+    }
+}
+function WriteSettings() {
+    // Create an instance of StreamWriter to write text to a file.
+	var    sw = new StreamWriter("Assets/Settings/Settings.txt");
+    // Add some text to the file.
+     for(var key : String in keys)
+    {
+        sw.WriteLine(key);
+    }
+     
+    sw.Close();
+}
+
