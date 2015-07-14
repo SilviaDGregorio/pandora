de9131b2cca5ccf4c4f33acef27589710ae3badd
unknown <ydril.linwelin@gmail.com>
2015-07-14 16:06:24 +0200

    exe

--- Assets/Scripts/ScriptsReader/Reader_js.js
+++ Assets/Scripts/ScriptsReader/Reader_js.js
@@ -13,6 +13,8 @@ var wood_material_light_off:Material;
 var rock_material:Material;
 var rock_material_light_on:Material;
 var rock_material_light_off:Material;
+var enemy180:Material;
+var enemy:Material;
 function Start ()
 {
 	var map=CSVReader.SplitCsvGrid(csv.text);
@@ -60,6 +62,7 @@ function Start ()
 					break;
 				case "5":
 				//	Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);
+					normal_enemy.GetComponent.<Renderer>().material=enemy;
 					Instantiate(normal_enemy as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0.4947718), Quaternion.Euler(Vector3(0,180,0)));
 					break;
 				case "6":
@@ -70,6 +73,12 @@ function Start ()
 				//	Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);
 					Instantiate(vertical_enemy as GameObject,  Vector3(xPosition*2,(yPosition*2)-8.3,0.4947718), Quaternion.Euler(Vector3(0,180,0)));
 					break;
+				case "8":
+				//	Debug.Log("pongo un"+position+"en x:"+xPosition+" y:"+yPosition);
+					normal_enemy.GetComponent.<Renderer>().material=enemy180;
+					Instantiate(normal_enemy as GameObject,  Vector3(xPosition*2,(yPosition*2)-8,0.4947718), Quaternion.Euler(Vector3(0,180,0)));
+					break;
+
 			}
 			
 		yPosition++;
